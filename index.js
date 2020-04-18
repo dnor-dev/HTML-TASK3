30949fe2a190d84eebb553fa8ed4c287d40a7553
dnor-dev <anorue4rss@hotmail.com>
2020-04-18 02:16:32 -0700

    phase done

---
+++ index.js
@@ -0,0 +1,33 @@
+var myButton = document.getElementById("mybtn");
+
+window.onscroll = function(){
+    scrollFunction();
+}
+
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        myButton.style.display = "block";
+    }else{
+        myButton.style.display = "none";
+    }
+}
+
+
+function topFunction(){
+    $("html , body").animate({scrollTop:"0"},900);
+}
+
+
+let bioData = $('#Name').position().top;
+
+function biodata(){
+    $("html , body").animate({scrollTop:bioData},700);
+}
+
+
+
+let conTact = $('#foot').position().top;
+
+function contact(){
+    $("html , body").animate({scrollTop:conTact},900)
+}
